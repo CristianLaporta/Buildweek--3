835234e17fd4aa91066b6094d254488c342270b9
eloismandotti <elois.mandotti.2f@gmail.com>
2022-07-12 17:12:36 +0200

    page404

--- src/app/err404/err404.module.ts
+++ src/app/err404/err404.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrPage } from './err/err.page';
 import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 const routes: Routes = [
   {
@@ -16,6 +18,8 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
+    MatIconModule,
+    MatButtonModule,
     RouterModule.forChild(routes)
   ]
 })
