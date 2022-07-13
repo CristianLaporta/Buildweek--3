60749dae2866e3d37c112bfb56f964e70dc34b5f
Cristian Laporta <37643992+TheCriicom@users.noreply.github.com>
2022-07-13 15:42:32 +0200

    fix

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,17 +5,20 @@ import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatListModule} from '@angular/material/list';
 import { VideobannerComponent } from './videobanner/videobanner.component';
-
+import {MatButtonModule} from '@angular/material/button';
+import { CardComponent } from './home/card/card.component';
 @NgModule({
   declarations: [
     HomePage,
-    VideobannerComponent
+    VideobannerComponent,
+    CardComponent
   ],
   imports: [
     CommonModule,
     MatCardModule,
     MatDividerModule,
-    MatListModule
+    MatListModule,
+    MatButtonModule
   ]
 })
 export class HomeModule { }
