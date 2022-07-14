3b69baca8053ac2858f091aca7420870e1161643
Cristian Laporta <37643992+TheCriicom@users.noreply.github.com>
2022-07-14 09:39:05 +0200

    fix

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,7 +7,6 @@ import {MatListModule} from '@angular/material/list';
 import { VideobannerComponent } from './videobanner/videobanner.component';
 import {MatButtonModule} from '@angular/material/button';
 import { CardComponent } from './home/card/card.component';
-import { SidebarComponent } from './sidebar/sidebar/sidebar.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatRadioModule} from '@angular/material/radio';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -18,7 +17,7 @@ import { AppRoutingModule } from '../app-routing.module';
     HomePage,
     VideobannerComponent,
     CardComponent,
-    SidebarComponent
+
 
   ],
   imports: [
---
+++ src/app/home/home/card/card.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/auth/auth.service';
+
+
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+  @ViewChild('f') form!:NgForm;
+prodotti:any = [];
+acces:boolean = false;
+name!:string;
+img!:string;
+price!:number;
+utente!:string;
+idutente!:number;
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router, private _snackBar: MatSnackBar ,private authService: AuthService ) { }
+
+  ngOnInit(): void {
+    this.ceck()
+    this.postGet()
+   
+  }
+
+  postGet() {
+    this.route.params.subscribe(ele => {
+        this.http.get("https://socialcris.duckdns.org:8445/api/prodotti").subscribe(Response => this.prodotti.push(Response));
+    }
+    )
+  }
+  openSnackBar(message: string) {
+    this._snackBar.open(message+ " "+'aggiunto al carrello!', "okay");
+
+  }
+  // verifica per il tasto acquista
+
+  ceck(){
+   if(localStorage.getItem('autenticated') == null){
+    this.acces = false
+   }else{
+    this.acces = true
+   }
+
+  }
+
+
+
+//sistema di verifica dei token taroccato
+
+
+
+addcart(name: string,img: string,price: number) {
+this.authService.authSubject.subscribe(val => {
+if (val !== null) {
+ fetch('https://socialcris.duckdns.org:8445/api/verify/'+ val)
+   .then((response) => response.json()).then((response) => {
+           
+    this.name = name;
+    this.img = img;
+    this.price = price;
+   this.utente = response[0].nickname;
+     this.idutente = response[0].iduser
+  this.addcartcomplete()
+    }
+    )}});
+    }
+    addcartcomplete() {
+      if (this.utente !== "") {
+        fetch("https://socialcris.duckdns.org:8445/api/carrello" + "?name=" + this.name + "&img=" + this.img + "&price=" + this.price + "&user=" + this.utente +"&idutente=" + this.idutente , {
+          method: "POST",
+          cache: 'no-cache'
+        }).then(function (response) { return console.log(response); });
+    
+      };
+    
+    }
+  }
+
+
+
+
+
