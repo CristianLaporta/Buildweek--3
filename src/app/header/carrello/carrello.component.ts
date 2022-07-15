cf10723f8a0a5c71095339d7b58a445cec3650f1
Cristian Laporta <37643992+TheCriicom@users.noreply.github.com>
2022-07-15 14:45:16 +0200

    fix

--- src/app/header/carrello/carrello.component.ts
+++ src/app/header/carrello/carrello.component.ts
@@ -11,6 +11,12 @@ import { AuthService } from 'src/app/auth/auth.service';
 export class CarrelloComponent implements OnInit {
   idutente!:number;
   carrellol:any = [];
+  totale:number= 0;
+  idrecuperato:any;
+  nome!:any;
+  cognome!:any;
+  email!:any;
+  indirizzo!:any;
   constructor(private authService: AuthService, private http: HttpClient, private route: ActivatedRoute) { }
   ngOnInit(): void {
    this.verifyuser()
@@ -22,12 +28,31 @@ export class CarrelloComponent implements OnInit {
      fetch('https://socialcris.duckdns.org:8446/api/verify/'+ val)
        .then((response) => response.json()).then((response) => {
          this.postGetl(response[0].iduser) 
+    this.idrecuperato = response[0].iduser;
+    this.nome = response[0].nome;
+    this.cognome = response[0].cognome;
+    this.email = response[0].email;
+    this.indirizzo = response[0].indirizzo;
         }
         )}})
         }
         postGetl(obj: string) {
           this.route.params.subscribe(ele => {
-              this.http.get("https://socialcris.duckdns.org:8446/api/carrello/?name="+obj).subscribe(Response => this.carrellol = Response);
+              this.http.get("https://socialcris.duckdns.org:8446/api/carrello/?name="+obj).subscribe(Response => {
+               
+                this.carrellol = Response;
+                let initialValue = 0
+                let totale = 0;
+                
+                while( initialValue < this.carrellol.length ) {
+                  let test = this.carrellol[initialValue].prezzo;
+                 let ciao = parseInt(test);
+                  totale += ciao;
+                  initialValue = initialValue +1
+                  this.totale = totale
+                }
+          });
+              
           }
           )
         }    
@@ -39,6 +64,23 @@ export class CarrelloComponent implements OnInit {
           setTimeout(() => {
             this.verifyuser()
           }, 500)
+       
+        }
+        
+        compra(obj: any){
+       console.log(this.totale);
+       fetch("https://socialcris.duckdns.org:8446/api/fatture" + "?nome=" + this.nome + "&cognome=" + this.cognome + "&email=" + this.email + "&indirizzo=" + this.indirizzo +"&idutente=" + this.idrecuperato+"&totale=" + this.totale , {
+        method: "POST",
+        cache: 'no-cache'
+      }).then(function (response) { return console.log(response); });
+
+        
+       this.route.params.subscribe(ele => {
+        this.http.delete("https://socialcris.duckdns.org:8446/api/carrelloremoveall/"+this.idrecuperato).subscribe(Response => console.log(Response));   
+        this.carrellol = [];
+        this.totale = 0;
+      })
+
        
         }
 }
